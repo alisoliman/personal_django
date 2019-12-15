8e2af9ba733e19d751b6d86da2f98900cf42410e
Ali Soliman <ali.soliman95@gmail.com>
2019-12-15 04:04:22 +0200

    Add profile settings to wagtail, add base css html and js

--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
-from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
+from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
 
 
 class HomePage(Page):
@@ -9,3 +11,18 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
     ]
+
+
+@register_setting
+class ProfileSettings(BaseSetting):
+    facebook = models.URLField(
+        help_text='Your Facebook page URL')
+    linkedin = models.URLField(
+        help_text='Your Linkedin page URL')
+    github = models.URLField(
+        help_text='Your Github page URL')
+    instagram = models.URLField(
+        help_text='Your Instagram page URL')
+    twitter = models.URLField(
+        help_text='Your Twitter page URL')
+    email = models.EmailField()
